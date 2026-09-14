5aac616ab85c4cbd5e23ff3ba2e546742b545ad6
DevDivyansh <576divyanshsharma@gmail.com>
2026-09-14 08:16:04 +0530

    feat: add functions to detect intersection and loop in linked lists

---
+++ LinkedList/faq medium/05.cpp
@@ -0,0 +1,19 @@
+/*
+Given the heads of two linked lists A and B, containing positive integers. Find the node at which the two linked lists intersect. If they do intersect, return the node at which the intersection begins, otherwise return null.
+
+
+
+The Linked List will not contain any cycles. The linked lists must retain their original structure, given as per the input, after the function returns.
+
+
+
+Note: for custom input, the following parameters are required(your program is not provided with these parameters):
+
+intersectVal - The value of the node where the intersection occurs. This is -1 if there is no intersected node.
+skipA - The number of nodes to skip ahead in listA (starting from the head) to get to the intersected node(-1 if no intersection).
+skipB - The number of nodes to skip ahead in listB (starting from the head) to get to the intersected node(-1 if no intersection).
+listA - The first linked list.
+listB - The second linked list.
+
+*/
+
